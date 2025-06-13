606d018a004faf01afcc3b980a77fa76d96b5532
Yen Ling <p21013562@student.newinti.edu.my>
2025-06-13 21:31:40 +0800

    update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'https://6003cem-web-api-client.vercel.app',
+  origin: 'https://6003-cem-web-api-client.vercel.app/',
   methods: ['GET', 'POST'],
   credentials: true,
 }));
