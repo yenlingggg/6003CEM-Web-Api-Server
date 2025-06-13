928a91302c1f135fb4ad3d48e679f27e65b25d4d
Yen Ling <p21013562@student.newinti.edu.my>
2025-06-14 00:02:28 +0800

    update

--- index.js
+++ index.js
@@ -17,11 +17,22 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
+const allowedOrigins = [
+  'https://6003cem-web-api-client-production.up.railway.app'
+];
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
 }));
+
 
 app.use('/api/auth',  authRoutes);
 app.use('/api/coins', coinRoutes);
@@ -34,11 +45,12 @@ const server = http.createServer(app);
 // Attach .io to same HTTP server
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL,
+    origin: 'https://6003cem-web-api-client-production.up.railway.app',
     methods: ['GET', 'POST'],
     credentials: true
   }
 });
+
 
 
 io.on('connection', (socket) => {
